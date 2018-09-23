e859751c11409fc57989fd60bd1a2b36c71c5110
Vitor Paes <paesvitor@outlook.com>
2018-09-23 13:48:05 -0300

    feat: Added posts list to dashboard & tweaked add forms

--- pages/dashboard-pages.js
+++ pages/dashboard-pages.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from "react";
 import Dashboard from "templates/dashboard";
-import styled, { css } from "styled-components";
 import {
     MdBrightness1 as StatusIcon,
     MdOpenInNew as LinkIcon
@@ -8,61 +7,9 @@ import {
 import { Alert } from "sagan-ui";
 import { pagesRef } from "modules/firebase";
 import Link from "next/link";
-
-const PageCard = styled.div`
-    ${({ theme }) => css`
-        border: 1px solid #e6ecf0;
-        padding: 1rem;
-        border-radius: 3px;
-        display: flex;
-        align-items: center;
-        border-left: 4px solid ${theme.primaryColor};
-        background-color: #f9f9f9;
-        margin-bottom: 1rem;
-        cursor: pointer;
-        transition: 0.2s all;
-
-        &:hover {
-            box-shadow: 0 1px 10px 4px rgba(0, 0, 0, 0),
-                0 1px 9px 0px rgba(0, 0, 0, 0.09);
-        }
-
-        .page-name {
-            flex: 1;
-            font-weight: 500;
-
-            .page-url {
-                padding-top: 0.3rem;
-                font-weight: 300;
-            }
-        }
-
-        .page-status {
-            display: flex;
-            align-items: center;
-            font-weight: 300;
-
-            svg {
-                padding-left: 1rem;
-                font-size: 1rem;
-
-                &:hover {
-                    color: ${theme.primaryColor};
-                }
-            }
-        }
-    `};
-`;
+import ListCard from "components/ListCard";
 
 class Pages extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            pages: {}
-        };
-    }
-
     static async getInitialProps(ctx) {
         const pages = await pagesRef
             .once("value")
@@ -82,18 +29,7 @@ class Pages extends React.Component {
                 actionUrl="/dashboard/add-page"
             >
                 {Object.keys(pages).map(key => (
-                    <PageCard>
-                        <div className="page-name">
-                            {pages[key].name}{" "}
-                            <div className="page-url">/{pages[key].url}</div>
-                        </div>
-                        <div className="page-status">
-                            Published{" "}
-                            <Link href={`/${pages[key].url}`}>
-                                <LinkIcon />
-                            </Link>
-                        </div>
-                    </PageCard>
+                    <ListCard item={pages[key]} key={key} slug={key} />
                 ))}
             </Dashboard>
         );
--- pages/dashboard-posts.js
+++ pages/dashboard-posts.js
@@ -1,15 +1,34 @@
-import React, { Component } from "react";
+import React, { Fragment } from "react";
 import Dashboard from "templates/dashboard";
+import {
+    MdBrightness1 as StatusIcon,
+    MdOpenInNew as LinkIcon
+} from "react-icons/md";
+import { postsRef } from "modules/firebase";
+import ListCard from "components/ListCard";
 
-export class DashboardPosts extends Component {
+class DashboardPosts extends React.Component {
+    static async getInitialProps(ctx) {
+        const posts = await postsRef
+            .once("value")
+            .then(snapshot => snapshot.val());
+        return {
+            posts
+        };
+    }
+
     render() {
+        const { posts } = this.props;
+
         return (
             <Dashboard
                 title="Posts"
                 action="Add new post"
                 actionUrl="/dashboard/add-post"
             >
-                posts
+                {Object.keys(posts).map(key => (
+                    <ListCard item={posts[key]} slug={key} key={key} />
+                ))}
             </Dashboard>
         );
     }
